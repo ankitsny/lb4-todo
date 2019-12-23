a8cbabb511395ec27bdf6eb5692f8d3e8fc41c4f
Ankit Kumar <ankit_sony3@hotmail.com>
2019-12-24 00:24:33 +0530

    initial commit

---
+++ src/models/todo-list.model.ts
@@ -0,0 +1,36 @@
+import {Entity, model, property, hasMany} from '@loopback/repository';
+import {Todo} from './todo.model';
+
+@model()
+export class TodoList extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  _id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+  })
+  color?: string;
+
+  @hasMany(() => Todo)
+  todos: Todo[];
+
+  constructor(data?: Partial<TodoList>) {
+    super(data);
+  }
+}
+
+export interface TodoListRelations {
+  // describe navigational properties here
+}
+
+export type TodoListWithRelations = TodoList & TodoListRelations;
---
+++ src/models/todo.model.ts
@@ -0,0 +1,41 @@
+import {Entity, model, property, belongsTo} from '@loopback/repository';
+import {TodoList} from './todo-list.model';
+
+@model()
+export class Todo extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  _id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+  })
+  desc?: string;
+
+  @property({
+    type: 'boolean',
+  })
+  isComplete?: boolean;
+
+  @belongsTo(() => TodoList)
+  todoListId: string;
+
+  constructor(data?: Partial<Todo>) {
+    super(data);
+  }
+}
+
+export interface TodoRelations {
+  // describe navigational properties here
+}
+
+export type TodoWithRelations = Todo & TodoRelations;
